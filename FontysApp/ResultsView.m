92c55727e673277718a985dc9e28f5c98ff1a9a0
Ruud Puts <ruud.puts@hotmail.com>
2011-08-24 22:38:27 +0200

    Tableview working.. kinda :')

--- FontysApp/ResultsView.m
+++ FontysApp/ResultsView.m
@@ -1,146 +1,95 @@
-//
-//  ResultsView.m
-//  FontysApp
-//
-//  Created by Ruud Puts on 8/24/11.
-//  Copyright 2011 Fontys Hogeschool ICT. All rights reserved.
-//
-
 #import "ResultsView.h"
+#import "Period.h"
+#import "Result.h"
 
 @implementation ResultsView
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
+@synthesize app;
 
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
+    app = (FontysAppAppDelegate *)[[UIApplication sharedApplication] delegate];
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    NSLog(@"periods: %d", [app.report.periods count]);
+    return [app.report.periods count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    NSLog(@"period: %@", ((Period *)[app.report.periods objectAtIndex:section]).description);
+    return [((Period*)[app.report.periods objectAtIndex:section]).results count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return ((Period *)[app.report.periods objectAtIndex:section]).description;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"ResultsCell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
     
-    // Configure the cell...
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    /*Result *currentResult = [[((Period*)[app.report.periods objectAtIndex:[indexPath section]]).results objectAtIndex:[indexPath row]] autorelease];
+    
+    [cell.textLabel setText:currentResult.course];
+    [cell.detailTextLabel setText:currentResult.description];*/
+    
+    UILabel *resultLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, 2, 40, 40)];
+    [resultLabel setFont:[UIFont fontWithName:@"Helvetica" size:30]];
+    [resultLabel setTextAlignment:UITextAlignmentCenter];
+    [cell addSubview:resultLabel];
+    
+    UILabel *courseLabel = [[UILabel alloc] initWithFrame:CGRectMake(44, 2, 276, 21)];
+    [courseLabel setFont:[UIFont fontWithName:@"Helvetica" size:17]];
+    [cell addSubview:courseLabel];
+    
+    UILabel *descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(44, 23, 276, 21)];
+    [descriptionLabel setFont:[UIFont fontWithName:@"Helvetica" size:14]];
+    [cell addSubview:descriptionLabel];
+    
+    Result *currentResult = [[((Period*)[app.report.periods objectAtIndex:[indexPath section]]).results objectAtIndex:[indexPath row]] autorelease];
+    if([[currentResult.result lowercaseString] isEqualToString:@"g"])
+    {
+        [resultLabel setTextColor:[UIColor greenColor]];
+        [resultLabel setText:@"G"];
+    } else if([[currentResult.result lowercaseString] isEqualToString:@"v"])
+    {
+        [resultLabel setTextColor:[UIColor greenColor]];
+        [resultLabel setText:@"V"];
+    } else if([[currentResult.result lowercaseString] isEqualToString:@"o"])
+    {
+        [resultLabel setTextColor:[UIColor redColor]];
+        [resultLabel setText:@"O"];
+    } else
+    {
+        double result = [currentResult.result doubleValue];
+        if (result < 5.5)
+            [resultLabel setTextColor:[UIColor redColor]];
+        else
+            [resultLabel setTextColor:[UIColor greenColor]];
+        [resultLabel setText:[NSString stringWithFormat:@"%d", result]];
+    }
+    [courseLabel setFont:cell.textLabel.font];
+    [courseLabel setText:currentResult.course];
+    [descriptionLabel setFont:cell.detailTextLabel.font];
+    [descriptionLabel setText:currentResult.description];
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
@@ -154,6 +103,12 @@
      [self.navigationController pushViewController:detailViewController animated:YES];
      [detailViewController release];
      */
+}
+
+- (void)dealloc
+{
+    [app release];
+    [super dealloc];
 }
 
 @end
